ab08966c51e693d0876c7babb0b7f502a8f61b36
LucasTeodoro2003 <lucasteodoro.lm@gmail.com>
2024-09-04 23:47:51 -0300

    InstalandoCursor

--- src/Widgets/header/index.tsx
+++ src/Widgets/header/index.tsx
@@ -8,7 +8,7 @@ function Header() {
         <div className='bg-cyan-50 dark:bg-black pt-4'>
             <div>
                 <img onClick={() => document.body.classList.toggle('dark')} src="https://s5.static.brasilescola.uol.com.br/be/2023/09/escudo-do-cruzeiro.jpg" alt="Escudo Cruzeiro" className='w-12 h-12 border-2 border-black rounded-full shadow-lg object-cover mr-4 ml-20 mb-4' />
-                <h2 className='text-black dark:text-cyan-500 font-bold text-center'>Teste HomePage Widgets</h2>
+                <h2 className='text-black dark:text-cyan-500 font-bold text-center'>Teste</h2>
             </div>
         </div>
     )
