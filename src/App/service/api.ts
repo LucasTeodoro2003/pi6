6cd6da756e731aeb6b65716fb2f646f33ada7c17
LucasTeodoro2003 <lucasteodoro.lm@gmail.com>
2024-10-07 18:43:26 -0300

    update alertEmployee

--- src/App/service/api.ts
+++ src/App/service/api.ts
@@ -1,15 +1,26 @@
 import axios from "axios";
 
 
-// PEOPLE 
+// PEOPLE COM FALTA DE EPI
 const api = axios.create({
-  baseURL: "https://webhook.site/2b27cb2c-325b-48c7-802a-0aaf766f3b4d",
+  baseURL: "https://webhook.site/59b971be-a19c-405c-aabc-512f4477b2bb",
   timeout: 1000,
 });
 const apiUser = axios.create({
   baseURL: "",
   timeout: 1000,
 })
+
+
+// PEOPLE TUDO CORRETO
+// const api = axios.create({
+//   baseURL: "https://webhook.site/7db9c146-695d-4334-8f0a-877c1171e60d",
+//   timeout: 1000,
+// });
+// const apiUser = axios.create({
+//   baseURL: "",
+//   timeout: 1000,
+// })
 
 // USER 
 // const apiUser = axios.create({
--- src/widgets/alertEmployee/ui/alertEmployee.tsx
+++ src/widgets/alertEmployee/ui/alertEmployee.tsx
@@ -1,4 +1,4 @@
-import { XCircleIcon } from '@heroicons/react/20/solid';
+import { CheckCircleIcon, FaceSmileIcon, XCircleIcon } from '@heroicons/react/20/solid';
 import React from 'react';
 import { Person } from '../../../Entities';
 
@@ -13,16 +13,16 @@ const AlertEmployee: React.FC<AlertEmployeePromps> = ({ people }) => {
   return (
     <div>
       {usingEpi === peopleCount ? (
-        <div className="rounded-md bg-green-50 p-4">
+        <div className="rounded-md bg-green-100 p-4 mr-32">
           <div className="flex">
             <div className="flex-shrink-0">
               <svg className="h-5 w-5 text-green-400" aria-hidden="true">
-                {/* Ícone de sucesso (pode substituir por um ícone adequado) */}
-                <circle cx="10" cy="10" r="8" fill="green" />
+                <CheckCircleIcon className='h-5 w-5 text-green-500' aria-hidden="true" />
               </svg>
             </div>
-            <div className="ml-3">
-              <h3 className="text-sm font-medium text-green-800">Está tudo correto! Todos estão usando EPI.</h3>
+            <div>
+              <h3 className="text-sm font-medium text-green-800">Está tudo correto! Todos estão usando as devidas EPI's.</h3>
+              <FaceSmileIcon className='mt-3 h-6 w-6'/>
             </div>
           </div>
         </div>
