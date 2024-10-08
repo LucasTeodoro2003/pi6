ad68d4667dab71698e077fdb26db2f4de58fe74f
LucasTeodoro2003 <lucasteodoro.lm@gmail.com>
2024-10-08 18:26:24 -0300

    init created PageLogin

--- src/App/routes/appRouter.tsx
+++ src/App/routes/appRouter.tsx
@@ -3,11 +3,13 @@ import { AlertPage } from "../../pages/alertas";
 import { ConfigPage } from "../../pages/config";
 import { FormularyPage } from "../../pages/formulary";
 import { HomePage } from "../../pages/home";
+import { Login } from "../../widgets/login";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />}/>
         <Route path="/" element={<HomePage />} />
         <Route path="/alertas" element={<AlertPage />} />
         <Route path="/Config" element={<ConfigPage />} />
